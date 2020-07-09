542aecc57e81e77bb5305386457c3cf088cfd329
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-07-09 14:11:11 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -8,18 +8,30 @@ class CreateTodo extends Component {
     text: '',
   };
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData
+    })
+  }
+}
  
 handleChange = event => {
     this.setState({
         text: event.target.value
     });
-}
+};
+
+handleSubmit = event => {
+  event.preventDefault();
+  this.props.addTodo(this.state);
+};
  
   render() {
     return(
       
 <div>
-        <form>
+        <form onSubmit={ event => this.handleSubmit(event) }>
           <p>
             <label>add todo<label>
             <input type="text" onChange={this.handleChange} value={this.state.text} />
